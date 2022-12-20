79585ac02bb3e7b74cde6b9b1efd030cf56e0f53
Gabriel Cuendet <gabriel.cuendet@gmail.com>
2022-12-20 22:51:03 +0100

    Add bufferline

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -2,4 +2,6 @@ vim.opt.expandtab = true                        -- convert tabs to spaces
 vim.opt.shiftwidth = 2                          -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 2                             -- insert 2 spaces for a tab
 vim.opt.number = true                           -- set numbered lines
+vim.opt.cursorline = true                       -- highlight the current line
+vim.opt.showtabline = 0                         -- always show tabs
 
